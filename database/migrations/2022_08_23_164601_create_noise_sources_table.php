c2199cf4fa2a327fa8616364dc959eb2278dae2d
Sergey Rukavichnikov <serega1998love@yandex.ru>
2022-08-23 22:50:20 +0500

    Добавил миграции и модели для приложения, запустил миграции

---
+++ database/migrations/2022_08_23_164601_create_noise_sources_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('noise_sources', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('check_source');
+            $table->string('name');
+            $table->string('mark')->nullable();
+            $table->float('distance')->nullable();
+            $table->float('31_5')->nullable();
+            $table->float('63')->nullable();
+            $table->float('125')->nullable();
+            $table->float('250')->nullable();
+            $table->float('500')->nullable();
+            $table->float('1000')->nullable();
+            $table->float('2000')->nullable();
+            $table->float('4000')->nullable();
+            $table->float('8000')->nullable();
+            $table->float('la_eq')->nullable();
+            $table->float('la_max')->nullable();
+            $table->text('foundation');
+            $table->text('remark')->nullable();
+            $table->unsignedBigInteger('id_file_path');
+            $table->unsignedBigInteger('id_type_of_source');
+            $table->unsignedBigInteger('id_user');
+
+            $table->foreign('id_user')->references('id')->on('users');
+            $table->foreign('id_file_path')->references('id')->on('file_noise_sources');
+            $table->foreign('id_type_of_source')->references('id')->on('type_noise_sources');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('noise_sources');
+    }
+};
