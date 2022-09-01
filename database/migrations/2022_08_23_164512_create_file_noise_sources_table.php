0dd00e09056801f14706833a66a4f283f187d703
Sergey Rukavichnikov <serega1998love@yandex.ru>
2022-09-01 21:46:32 +0500

    Сделал поле для наименования файла в БД уникальным

--- database/migrations/2022_08_23_164512_create_file_noise_sources_table.php
+++ database/migrations/2022_08_23_164512_create_file_noise_sources_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('file_noise_sources', function (Blueprint $table) {
             $table->id();
-            $table->string('file_name');
+            $table->string('file_name')->unique();
         });
     }
 
