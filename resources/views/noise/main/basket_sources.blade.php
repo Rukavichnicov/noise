298078855184f33cc105455d1e5825497a7d5bb9
Sergey Rukavichnikov <serega1998love@yandex.ru>
2022-10-05 22:11:37 +0500

    Изменил отображение пустого, личного списка пользователя

--- resources/views/noise/main/basket_sources.blade.php
+++ resources/views/noise/main/basket_sources.blade.php
@@ -7,7 +7,12 @@
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-body">
-                        @include('noise.main.includes.table_sources_users')
+                        @if(empty($paginator->total()))
+                            <p class="lead text-center">Вы пока не добавили в список ни одного источника шума.
+                                Чтобы добавить, зайдите на вкладку "Источники шума".</p>
+                        @else
+                            @include('noise.main.includes.table_sources_users')
+                        @endif
                     </div>
                 </div>
             </div>
