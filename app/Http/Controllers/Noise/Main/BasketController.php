46965fc23b504e8e20b4d850c1f531d17e5bccb4
Sergey Rukavichnikov <serega1998love@yandex.ru>
2022-10-30 22:34:35 +0500

    Сделал функцию вывода выбранных ИШ в Word

--- app/Http/Controllers/Noise/Main/BasketController.php
+++ app/Http/Controllers/Noise/Main/BasketController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\BasketCreateRequest;
 use App\Repositories\BasketRepository;
 use Exception;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Str;
 use Illuminate\View\View;
 
 class BasketController extends MainController
@@ -75,30 +76,64 @@ class BasketController extends MainController
     public function downloadReport()
     {
         $data = $this->basketRepository->getAllSourcesInBasket();
-        $array = [];
+
         $phpWord = new \PhpOffice\PhpWord\PhpWord();
-        $section = $phpWord->addSection();
+
+        $sectionStyle = array('orientation' => 'landscape',
+            'marginLeft' => 600, //Левое поле равно 15 мм
+            'marginRight' => 600,
+            'marginTop' => 600,
+        );
+        $styleTable = [
+            'borderSize' => 10,
+            'align' => 'center'
+        ];
+        $cellHCentered = array('alignment' => \PhpOffice\PhpWord\SimpleType\Jc::CENTER);
+        $cellVCentered = array('valign' => 'center');
+
+        $section = $phpWord->addSection($sectionStyle);
+        $text = "Список источников шума";
+        $fontStyle = array('name' => 'Times New Roman', 'size' => 12, 'color' => '000000', 'lang' => 'ru-RU');
+        $parStyle = array('align' => 'center', 'spaceBefore' => 10);
+        $section->addText(htmlspecialchars($text), $fontStyle, $parStyle);
+
+        $fontStyleTable = array('name' => 'Times New Roman', 'size' => 10, 'color' => '000000', 'lang' => 'ru-RU', );
+        $table = $section->addTable($styleTable);
+
+        $arrHeaderTable = array("№ ИШ", "Наименование источника шума", "Марка", "Дистанция замера", "31.5", "63", "125",
+            "250", "500", "1000", "2000", "4000", "8000", "La экв", "La макс", "Обоснование", "Примечание");
+
+        $table->addRow(1000);
+        foreach ($arrHeaderTable as $header) {
+            $cell = $table->addCell(900, $cellVCentered)->addText($header, $fontStyleTable, $cellHCentered);
+        }
+        $numberSourcesInTable = 1;
+
         foreach ($data as $item) {
-            $text = $section->addText($item->noiseSource->name);
-            $text = $section->addText($item->noiseSource->mark);
-            $text = $section->addText($item->noiseSource->distance);
-            $text = $section->addText($item->noiseSource->la_31_5);
-            $text = $section->addText($item->noiseSource->la_63);
-            $text = $section->addText($item->noiseSource->la_125);
-            $text = $section->addText($item->noiseSource->la_250);
-            $text = $section->addText($item->noiseSource->la_500);
-            $text = $section->addText($item->noiseSource->la_1000);
-            $text = $section->addText($item->noiseSource->la_2000);
-            $text = $section->addText($item->noiseSource->la_4000);
-            $text = $section->addText($item->noiseSource->la_8000);
-            $text = $section->addText($item->noiseSource->la_eq);
-            $text = $section->addText($item->noiseSource->la_max);
-            $text = $section->addText($item->fileNoiseSource->foundation);
-            $text = $section->addText($item->noiseSource->remark);
+            $table->addRow(1000);
+            $table->addCell(900, $cellVCentered)->addText($numberSourcesInTable, $fontStyleTable, $cellHCentered);
+            $table->addCell(900, $cellVCentered)->addText($item->noiseSource->name, $fontStyleTable, $cellHCentered);
+            $table->addCell(900, $cellVCentered)->addText($item->noiseSource->mark, $fontStyleTable, $cellHCentered);
+            $table->addCell(900, $cellVCentered)->addText($item->noiseSource->distance, $fontStyleTable, $cellHCentered);
+            $table->addCell(900, $cellVCentered)->addText($item->noiseSource->la_31_5, $fontStyleTable, $cellHCentered);
+            $table->addCell(900, $cellVCentered)->addText($item->noiseSource->la_63, $fontStyleTable, $cellHCentered);
+            $table->addCell(900, $cellVCentered)->addText($item->noiseSource->la_125, $fontStyleTable, $cellHCentered);
+            $table->addCell(900, $cellVCentered)->addText($item->noiseSource->la_250, $fontStyleTable, $cellHCentered);
+            $table->addCell(900, $cellVCentered)->addText($item->noiseSource->la_500, $fontStyleTable, $cellHCentered);
+            $table->addCell(900, $cellVCentered)->addText($item->noiseSource->la_1000, $fontStyleTable, $cellHCentered);
+            $table->addCell(900, $cellVCentered)->addText($item->noiseSource->la_2000, $fontStyleTable, $cellHCentered);
+            $table->addCell(900, $cellVCentered)->addText($item->noiseSource->la_4000, $fontStyleTable, $cellHCentered);
+            $table->addCell(900, $cellVCentered)->addText($item->noiseSource->la_8000, $fontStyleTable, $cellHCentered);
+            $table->addCell(900, $cellVCentered)->addText($item->noiseSource->la_eq, $fontStyleTable, $cellHCentered);
+            $table->addCell(900, $cellVCentered)->addText($item->noiseSource->la_max, $fontStyleTable, $cellHCentered);
+            $table->addCell(900, $cellVCentered)->addText($item->fileNoiseSource->foundation, $fontStyleTable, $cellHCentered);
+            $table->addCell(900, $cellVCentered)->addText($item->noiseSource->remark, $fontStyleTable, $cellHCentered);
+            $numberSourcesInTable++;
         }
         $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
-        $objWriter->save('Appdividend.docx');
-        return response()->download(public_path('Appdividend.docx'));
+        $fileName = Str::random(10) . '.docx';
+        $objWriter->save($fileName);
+        return response()->download(public_path($fileName))->deleteFileAfterSend();
     }
 
     /**
