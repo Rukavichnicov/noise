f3e05013c7003f7a10260e7e1871e2ffea860a3d
Sergey Rukavichnikov <serega1998love@yandex.ru>
2022-12-02 21:46:47 +0500

    Написал тесты проверяющий сохранение источников шума

--- tests/Feature/MainNoiseSourcesTest.php
+++ tests/Feature/MainNoiseSourcesTest.php
@@ -12,32 +12,72 @@ class MainNoiseSourcesTest extends TestCase
     public function test_store_one_noise_sources()
     {
         $user = $this->createUsualUser();
-        $path = public_path('foundation1.pdf');
-        $name = 'foundation1.pdf';
-        $file_pdf = new UploadedFile($path, $name, 'application/pdf', 0, true);
+        $arrayDataOneNoiseSource = $this->createValidDataOneNoiseSource();
 
-        $response = $this->actingAs($user)->post('/noise/main/sources', [
-              "count" => "1",
-              "name_1" => "ИсточникТест",
-              "mark_1" => "МаркаТест",
-              "id_type_of_source_1" => "1",
-              "distance_1" => "1",
-              "la_31_5_1" => "50",
-              "la_63_1" => "50",
-              "la_125_1" => "50",
-              "la_250_1" => "50",
-              "la_500_1" => "50",
-              "la_1000_1" => "50",
-              "la_2000_1" => "50",
-              "la_4000_1" => "50",
-              "la_8000_1" => "50",
-              "la_eq_1" => "80",
-              "la_max_1" => "80",
-              "remark_1" => "ПримечаниеТест",
-              "foundation" => "Обоснование Тест",
-              "submit" => "Добавить в базу данных",
-              "file_name" => $file_pdf
-            ]);
+        $response = $this->actingAs($user)->post('/noise/main/sources', $arrayDataOneNoiseSource);
         $response->assertRedirect('noise/main/sources');
+    }
+
+    public function test_store_two_noise_sources()
+    {
+        $user = $this->createUsualUser();
+        $arrayDataTwoNoiseSource = $this->createValidDataOneNoiseSource();
+        $arrayDataTwoNoiseSource['count'] = '2';
+        $arrayDataTwoNoiseSource = array_merge($arrayDataTwoNoiseSource, [
+            "name_2" => "ИсточникТест2",
+            "mark_2" => "МаркаТест2",
+            "id_type_of_source_2" => "2",
+            "distance_2" => "2",
+            "la_31_5_2" => "50",
+            "la_63_2" => "50",
+            "la_125_2" => "50",
+            "la_250_2" => "50",
+            "la_500_2" => "50",
+            "la_1000_2" => "50",
+            "la_2000_2" => "50",
+            "la_4000_2" => "50",
+            "la_8000_2" => "50",
+            "la_eq_2" => "70",
+            "la_max_2" => "70",
+            "remark_2" => "ПримечаниеТест2",
+        ]);
+
+        $response = $this->actingAs($user)->post('/noise/main/sources', $arrayDataTwoNoiseSource);
+        $response->assertRedirect('noise/main/sources');
+    }
+
+    public function test_store_one_noise_sources_without_file_is_invalid()
+    {
+        $user = $this->createUsualUser();
+        $arrayDataOneNoiseSource = $this->createValidDataOneNoiseSource();
+        $arrayDataOneNoiseSource['file_name'] = '';
+
+        $response = $this->actingAs($user)->post('/noise/main/sources', $arrayDataOneNoiseSource);
+
+        $response->assertRedirect();
+        $response->assertInvalid(['file_name']);
+    }
+
+    public function test_store_one_noise_sources_without_name_is_invalid()
+    {
+        $user = $this->createUsualUser();
+        $arrayDataOneNoiseSource = $this->createValidDataOneNoiseSource();
+        $arrayDataOneNoiseSource['name_1'] = '';
+
+        $response = $this->actingAs($user)->post('/noise/main/sources', $arrayDataOneNoiseSource);
+        $response->assertRedirect();
+        $response->assertInvalid(['name_1']);
+    }
+
+    public function test_store_one_noise_sources_without_foundation_is_invalid()
+    {
+        $user = $this->createUsualUser();
+        $arrayDataOneNoiseSource = $this->createValidDataOneNoiseSource();
+        $arrayDataOneNoiseSource['foundation'] = '';
+
+        $response = $this->actingAs($user)->post('/noise/main/sources', $arrayDataOneNoiseSource);
+
+        $response->assertRedirect();
+        $response->assertInvalid(['foundation']);
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,7 @@ namespace Tests;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Hash;
 
 abstract class TestCase extends BaseTestCase
@@ -28,5 +29,41 @@ abstract class TestCase extends BaseTestCase
         $user->password = Hash::make('password');
         $user->id_role = USUAL_USER;
         return $user;
+    }
+
+    public function createValidFoundationFile(): UploadedFile
+    {
+        $path = public_path('foundation1.pdf');
+        $name = 'foundation1.pdf';
+        $file_pdf = new UploadedFile($path, $name, 'application/pdf', 0, true);
+        return $file_pdf;
+    }
+
+    public function createValidDataOneNoiseSource(): array
+    {
+        $file_pdf = $this->createValidFoundationFile();
+        $arrayDataOneNoiseSource = [
+            "count" => "1",
+            "name_1" => "BТест",
+            "mark_1" => "МаркаТест",
+            "id_type_of_source_1" => "1",
+            "distance_1" => "1",
+            "la_31_5_1" => "50",
+            "la_63_1" => "50",
+            "la_125_1" => "50",
+            "la_250_1" => "50",
+            "la_500_1" => "50",
+            "la_1000_1" => "50",
+            "la_2000_1" => "50",
+            "la_4000_1" => "50",
+            "la_8000_1" => "50",
+            "la_eq_1" => "80",
+            "la_max_1" => "80",
+            "remark_1" => "ПримечаниеТест",
+            "foundation" => "ОбоснованиеТест",
+            "submit" => "Добавить в базу данных",
+            "file_name" => $file_pdf,
+        ];
+        return $arrayDataOneNoiseSource;
     }
 }
