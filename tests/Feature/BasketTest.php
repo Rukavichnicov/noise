9f028dd396af4e885213834fcb0794a186e843cc
Sergey Rukavichnikov <serega1998love@yandex.ru>
2022-12-06 20:44:43 +0500

    Добавил тесты для проверки удаления ИШ из корзины

--- tests/Feature/BasketTest.php
+++ tests/Feature/BasketTest.php
@@ -20,12 +20,13 @@ class BasketTest extends TestCase
     public function test_adding_sources_in_basket()
     {
         $user = $this->createUsualUser();
+        $user->id = 2;
 
         $response = $this->actingAs($user)->post('noise/main/basket', [
             "addSources" => "93"
         ]);
 
-        $response->assertRedirect();
+        $response->assertRedirect('/');
 
         Basket::query()->where('id_user', '=', $user->id)->delete();
     }
@@ -33,6 +34,7 @@ class BasketTest extends TestCase
     public function test_double_adding_sources_in_basket_is_invalid()
     {
         $user = $this->createUsualUser();
+        $user->id = 2;
 
         $response = $this->actingAs($user)->post('noise/main/basket', [
             "addSources" => "93"
@@ -44,5 +46,35 @@ class BasketTest extends TestCase
         $response->assertInvalid(['addSources']);
 
         Basket::query()->where('id_user', '=', $user->id)->delete();
+    }
+
+    public function test_deleting_sources_from_basket_without_authorization_is_invalid()
+    {
+        $response = $this->delete('noise/main/basket/93');
+
+        $response->assertRedirect('/login');
+    }
+
+    public function test_deleting_sources_from_basket()
+    {
+        $user = $this->createUsualUser();
+        $user->id = 2;
+        Basket::query()->insert(['id_user' => $user->id, 'id_noise_source' => '93']);
+
+        $response = $this->actingAs($user)->delete('noise/main/basket/93');
+
+        $response->assertRedirect('/');
+    }
+
+    public function test_double_deleting_sources_from_basket_is_invalid()
+    {
+        $user = $this->createUsualUser();
+        $user->id = 2;
+        Basket::query()->insert(['id_user' => $user->id, 'id_noise_source' => '93']);
+
+        $response = $this->actingAs($user)->delete('noise/main/basket/93');
+        $response = $this->actingAs($user)->delete('noise/main/basket/93');
+
+        $response->assertSessionHasErrors();
     }
 }
