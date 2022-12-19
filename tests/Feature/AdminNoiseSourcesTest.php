725d512385d92cbffbfcc92225907d33b013687b
Sergey Rukavichnikov <serega1998love@yandex.ru>
2022-12-19 21:01:10 +0500

    Добавил тест для проверки редактирования источника шума

--- tests/Feature/AdminNoiseSourcesTest.php
+++ tests/Feature/AdminNoiseSourcesTest.php
@@ -87,4 +87,73 @@ class AdminNoiseSourcesTest extends TestCase
         $response->assertSessionHasErrors();
         $response->assertRedirect('noise/admin/sources');
     }
+
+    public function test_update_noise_source()
+    {
+        $user = $this->createAdminUser();
+        $oneSource = NoiseSource::query()->find(1)->first();
+        $fileSource = FileNoiseSource::query()->find($oneSource->id_file_path)->first();
+        $nameChanged = 'Источник 1';
+
+        $response = $this->actingAs($user)->from("noise/admin/sources/$oneSource->id/edit")->patch("noise/admin/sources/$oneSource->id", [
+            "name" => $nameChanged,
+            "mark" => $oneSource->mark,
+            "id_type_of_source" => $oneSource->id_type_of_source,
+            "distance" => $oneSource->distance,
+            "la_31_5" => $oneSource->la_31_5,
+            "la_63" => $oneSource->la_63,
+            "la_125" => $oneSource->la_125,
+            "la_250" => $oneSource->la_250,
+            "la_500" => $oneSource->la_500,
+            "la_1000" => $oneSource->la_1000,
+            "la_2000" => $oneSource->la_2000,
+            "la_4000" => $oneSource->la_4000,
+            "la_8000" => $oneSource->la_8000,
+            "la_eq" => $oneSource->la_eq,
+            "la_max" => $oneSource->la_max,
+            "remark" => $oneSource->remark,
+            "foundation" => $fileSource->foundation,
+        ]);
+
+        $sourceAfterChanged = NoiseSource::query()->find(1)->first();
+        $response->assertSessionHasNoErrors();
+        $response->assertRedirect(route('noise.admin.sources.index'));
+        $this->assertSame($nameChanged, $sourceAfterChanged->name);
+
+
+        $sourceAfterChanged->update(['name' => 'Сварочное оборудование']);
+    }
+
+    public function test_update_noise_source_is_invalid_without_name()
+    {
+        $user = $this->createAdminUser();
+        $oneSource = NoiseSource::query()->find(1)->first();
+        $fileSource = FileNoiseSource::query()->find($oneSource->id_file_path)->first();
+        $fromUri = "noise/admin/sources/$oneSource->id/edit";
+        $mark = 'Mark';
+
+        $response = $this->actingAs($user)->from($fromUri)->patch("noise/admin/sources/$oneSource->id", [
+            "mark" => $mark,
+            "id_type_of_source" => $oneSource->id_type_of_source,
+            "distance" => $oneSource->distance,
+            "la_31_5" => $oneSource->la_31_5,
+            "la_63" => $oneSource->la_63,
+            "la_125" => $oneSource->la_125,
+            "la_250" => $oneSource->la_250,
+            "la_500" => $oneSource->la_500,
+            "la_1000" => $oneSource->la_1000,
+            "la_2000" => $oneSource->la_2000,
+            "la_4000" => $oneSource->la_4000,
+            "la_8000" => $oneSource->la_8000,
+            "la_eq" => $oneSource->la_eq,
+            "la_max" => $oneSource->la_max,
+            "remark" => $oneSource->remark,
+            "foundation" => $fileSource->foundation,
+        ]);
+
+        $sourceAfterChanged = NoiseSource::query()->find(1)->first();
+        $response->assertSessionHasErrors();
+        $response->assertRedirect($fromUri);
+        $this->assertNotSame($mark, $sourceAfterChanged->mark);
+    }
 }
